de6bf9732d2aa200ebdec177e3695d3866a967df
Nick Feichtel <nfeichtel@gmail.com>
2024-12-03 14:46:29 -0700

    load data

--- setup_queries/load_data.sql
+++ setup_queries/load_data.sql
@@ -24,3 +24,12 @@ COPY products
 FROM '/Users/nickfeichtel/Desktop/Portfolio_Projects/instacart_data/products.csv'
 DELIMITER ',' CSV HEADER;
 
+
+/*
+\copy aisles FROM '/Users/nickfeichtel/Desktop/Portfolio_Projects/instacart_data/aisles.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy department FROM '/Users/nickfeichtel/Desktop/Portfolio_Projects/instacart_data/departments.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy order_products_prior FROM '/Users/nickfeichtel/Desktop/Portfolio_Projects/instacart_data/order_products__prior.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy order_products_curr FROM '/Users/nickfeichtel/Desktop/Portfolio_Projects/instacart_data/order_products__train.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy orders FROM '/Users/nickfeichtel/Desktop/Portfolio_Projects/instacart_data/orders.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy products FROM '/Users/nickfeichtel/Desktop/Portfolio_Projects/instacart_data/products.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+*/

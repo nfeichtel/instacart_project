d831b698e8612d65dca142d17c2d21f4f5d853c2
Nick Feichtel <nfeichtel@gmail.com>
2024-12-12 15:44:51 -0700

    create missing order query

---
+++ analysis_queries/missing_orders.sql
@@ -0,0 +1,11 @@
+-- Calculate the percent of missing reorder data for each quarter
+SELECT
+    ROUND((SELECT SUM(q2_reordered) AS q2_not_missing
+        FROM total_reorders_augmented
+        WHERE aisle = 'missing') / SUM(q2_reordered) * 100, 2) AS q2_percent_missing,
+    ROUND((SELECT SUM(q3_reordered) AS q3_not_missing
+        FROM total_reorders_augmented
+        WHERE aisle = 'missing') / SUM(q3_reordered) * 100, 2) AS q3_percent_missing   
+FROM total_reorders_augmented
+
+

9fbed2446e31b79c30d038dec300c89338c2a43b
Nick Feichtel <nfeichtel@gmail.com>
2025-01-07 13:18:49 -0700

    added quarterly percent reorders

--- analysis_queries/03_segmentation_aisle.sql
+++ analysis_queries/03_segmentation_aisle.sql
@@ -6,7 +6,10 @@ SELECT
     SUM(q2_reordered) AS q2,
     SUM(q3_reordered) AS q3,
     SUM(q3_reordered) - SUM(q2_reordered) AS difference,
-    ROUND((SUM(q3_reordered) - SUM(q2_reordered)) / SUM(q2_reordered) * 100, 2) AS percent_change
+    ROUND((SUM(q3_reordered) - SUM(q2_reordered)) / SUM(q2_reordered) * 100, 2) AS percent_change,
+    ROUND(SUM(q2_reordered) / (SELECT SUM(q2_reordered) FROM (SELECT * FROM total_reorders_augmented WHERE aisle != 'missing')) * 100, 2) AS per_q2_reorders,
+    ROUND(SUM(q3_reordered) / (SELECT SUM(q3_reordered) FROM (SELECT * FROM total_reorders_augmented WHERE aisle != 'missing')) * 100, 2) AS per_q3_reorders
 FROM (SELECT * FROM total_reorders_augmented WHERE aisle != 'missing')
 GROUP BY aisle
-ORDER BY ROUND((SUM(q3_reordered) - SUM(q2_reordered)) / SUM(q2_reordered) * 100, 2)
+--ORDER BY ROUND((SUM(q3_reordered) - SUM(q2_reordered)) / SUM(q2_reordered) * 100, 2)
+ORDER BY per_q2_reorders DESC
--- analysis_queries/04_segmentation_product.sql
+++ analysis_queries/04_segmentation_product.sql
@@ -3,12 +3,16 @@ SELECT
     SUM(q2_reordered) AS q2,
     SUM(q3_reordered) AS q3,
     SUM(q3_reordered) - SUM(q2_reordered) AS difference,
-    ROUND((SUM(q3_reordered) - SUM(q2_reordered)) / SUM(q2_reordered) * 100, 2) AS percent_change
+    ROUND((SUM(q3_reordered) - SUM(q2_reordered)) / SUM(q2_reordered) * 100, 2) AS percent_change,
+    ROUND(SUM(q2_reordered) / (SELECT SUM(q2_reordered) FROM (SELECT * FROM total_reorders_augmented WHERE aisle != 'missing')) * 100, 2) AS per_q2_reorders,
+    ROUND(SUM(q3_reordered) / (SELECT SUM(q3_reordered) FROM (SELECT * FROM total_reorders_augmented WHERE aisle != 'missing')) * 100, 2) AS per_q3_reorders
 FROM (SELECT * FROM total_reorders_augmented WHERE aisle != 'missing' AND department != 'missing')
 GROUP BY product_name
 HAVING SUM(q2_reordered) != 0
---ORDER BY ROUND((SUM(q3_reordered) - SUM(q2_reordered)) / SUM(q2_reordered) * 100, 2)
-ORDER BY SUM(q3_reordered) - SUM(q2_reordered) DESC
+ORDER BY per_q2_reorders DESC
 
 -- a lot of large reorder differences are organic products. Was there an increase in price that led to this drop off?
 -- Look to see if any products containing "organic" had a positive difference
+-- 17 of 20 top reordered products are produce
+-- 15 of top 20 products are organic
+-- some of the hass avocado sales could have gone to the regular avocado sales

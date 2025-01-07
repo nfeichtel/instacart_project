41d9c637af6123636638764cb1a1f00de72e4c88
Nick Feichtel <nfeichtel@gmail.com>
2025-01-07 13:33:04 -0700

    added comments

--- analysis_queries/04_segmentation_product.sql
+++ analysis_queries/04_segmentation_product.sql
@@ -9,10 +9,13 @@ SELECT
 FROM (SELECT * FROM total_reorders_augmented WHERE aisle != 'missing' AND department != 'missing')
 GROUP BY product_name
 HAVING SUM(q2_reordered) != 0
-ORDER BY per_q2_reorders DESC
+ORDER BY  difference ASC
+LIMIT 20
 
--- a lot of large reorder differences are organic products. Was there an increase in price that led to this drop off?
--- Look to see if any products containing "organic" had a positive difference
+-- a lot of large reorder differences are organic products -> was there an increase in price that led to this drop off?
+-- Look to see if any products containing "organic" had a positive difference -> 8 of top 20 positive differences were organic
+-- 15 of top 20 negative differences were organic products
 -- 17 of 20 top reordered products are produce
 -- 15 of top 20 products are organic
 -- some of the hass avocado sales could have gone to the regular avocado sales
+-- Have finance team add pricing info to data. Want to see if pricing is major driving force for produce and organic produce reorders

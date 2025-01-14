d8b4abc6cc5d85b85a0aa29a5f72c4508164072d
Nick Feichtel <nfeichtel@gmail.com>
2025-01-14 14:56:33 -0700

    create yogurt query

--- analysis_queries/deep_dive_dairyeggs_yogurt.sql
+++ analysis_queries/deep_dive_dairyeggs_yogurt.sql
@@ -1,8 +1,4 @@
--- Investigating the large percent decrease in baby department reorders
--- Babies: 98.5% of the decrease came from a drop in baby food formula reorders
--- Bulk:
--- Alcohol:
--- Dairy eggs:
+
 
 SELECT
     aisle,
@@ -10,19 +6,19 @@ SELECT
     SUM(q2_reordered) AS q2,
     SUM(q3_reordered) AS q3,
     SUM(q3_reordered) - SUM(q2_reordered) AS product_difference
-FROM (SELECT * FROM total_reorders_augmented WHERE aisle != 'missing' AND department = 'dairy eggs' AND aisle = 'cream')
+FROM (SELECT * FROM total_reorders_augmented WHERE aisle != 'missing' AND department = 'dairy eggs' AND aisle = 'yogurt')
 GROUP BY aisle, product_name
 ORDER BY product_difference;
 
 -- percent difference of negative products
-WITH negative_cream AS
+WITH negative_yogurt AS
     (SELECT
         aisle,
         product_name,
         SUM(q2_reordered) AS q2,
         SUM(q3_reordered) AS q3,
         SUM(q3_reordered) - SUM(q2_reordered) AS product_difference
-    FROM (SELECT * FROM total_reorders_augmented WHERE aisle != 'missing' AND department = 'dairy eggs' AND aisle = 'cream')
+    FROM (SELECT * FROM total_reorders_augmented WHERE aisle != 'missing' AND department = 'dairy eggs' AND aisle = 'yogurt')
     GROUP BY 
         aisle, 
         product_name
@@ -33,34 +29,34 @@ SELECT
     aisle,
     product_name,
     product_difference,
-    ROUND(product_difference / (SELECT SUM(product_difference) FROM negative_cream) * 100, 2)
-FROM negative_cream;
+    ROUND(product_difference / (SELECT SUM(product_difference) FROM negative_yogurt) * 100, 2)
+FROM negative_yogurt;
 
 
 -- organic analysis
--- only 4 negative organic cream products
-WITH negative_cream AS
+
+WITH negative_yogurt AS
     (SELECT
         aisle,
         product_name,
         SUM(q2_reordered) AS q2,
         SUM(q3_reordered) AS q3,
         SUM(q3_reordered) - SUM(q2_reordered) AS product_difference
-    FROM (SELECT * FROM total_reorders_augmented WHERE aisle != 'missing' AND department = 'dairy eggs' AND aisle = 'cream')
+    FROM (SELECT * FROM total_reorders_augmented WHERE aisle != 'missing' AND department = 'dairy eggs' AND aisle = 'yogurt')
     GROUP BY 
         aisle, 
         product_name
     HAVING  SUM(q3_reordered) - SUM(q2_reordered) < 0
     ORDER BY product_difference),
 
-    organic_cream AS
+    organic_yogurt AS
     (SELECT
         aisle,
         product_name,
         SUM(q2_reordered) AS q2,
         SUM(q3_reordered) AS q3,
         SUM(q3_reordered) - SUM(q2_reordered) AS product_difference
-    FROM (SELECT * FROM total_reorders_augmented WHERE aisle != 'missing' AND department = 'dairy eggs' AND aisle = 'cream' AND product_name ILIKE '%organic%')
+    FROM (SELECT * FROM total_reorders_augmented WHERE aisle != 'missing' AND department = 'dairy eggs' AND aisle = 'yogurt' AND product_name ILIKE '%organic%')
     GROUP BY 
         aisle, 
         product_name
@@ -69,6 +65,6 @@ WITH negative_cream AS
 
 SELECT
     aisle,
-    ROUND((SELECT SUM(product_difference) FROM organic_cream) / (SELECT SUM(product_difference) FROM negative_cream) * 100, 2) AS percent_organic
-FROM negative_cream
+    ROUND((SELECT SUM(product_difference) FROM organic_yogurt) / (SELECT SUM(product_difference) FROM negative_yogurt) * 100, 2) AS percent_organic
+FROM negative_yogurt
 GROUP BY aisle

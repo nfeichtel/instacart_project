3c6bbd57c7b526ca2f38a4ae66c0aaac67f99635
Nick Feichtel <nfeichtel@gmail.com>
2025-01-17 14:49:51 -0700

    added aisle analysis queries, added product count query

---
+++ analysis_queries/organic_vs_non_frozen.sql
@@ -0,0 +1,54 @@
+
+WITH organic_aisle AS
+    (SELECT
+        aisle AS aisle_organic,
+        SUM(q2_reordered) AS q2_organic,
+        SUM(q3_reordered) AS q3_organic,
+        SUM(q3_reordered) - SUM(q2_reordered) organic_difference,
+        CASE WHEN SUM(q2_reordered) != 0 THEN ROUND((SUM(q3_reordered) - SUM(q2_reordered)) / SUM(q2_reordered) * 100, 2)
+            ELSE NULL
+        END AS organic_percent_change
+    FROM(SELECT * FROM total_reorders_augmented WHERE aisle != 'missing' AND product_name ILIKE '%organic%')
+    GROUP BY aisle
+    ORDER BY organic_difference),
+
+    non_organic_aisle AS
+    (SELECT
+        aisle AS aisle_non_organic,
+        SUM(q2_reordered) AS q2_non_organic,
+        SUM(q3_reordered) AS q3_non_organic,
+        SUM(q3_reordered) - SUM(q2_reordered) non_organic_difference,
+        CASE WHEN SUM(q2_reordered) != 0 THEN ROUND((SUM(q3_reordered) - SUM(q2_reordered)) / SUM(q2_reordered) * 100, 2)
+            ELSE NULL
+        END AS non_organic_percent_change
+    FROM(SELECT * FROM total_reorders_augmented WHERE aisle != 'missing' AND product_name NOT ILIKE '%organic%')
+    GROUP BY aisle
+    ORDER BY non_organic_difference),
+
+    total_aisle AS
+    (SELECT
+        aisle AS aisle_total,
+        SUM(q2_reordered) AS q2_total,
+        SUM(q3_reordered) AS q3_total,
+        SUM(q3_reordered) - SUM(q2_reordered) total_difference,
+        CASE WHEN SUM(q2_reordered) != 0 THEN ROUND((SUM(q3_reordered) - SUM(q2_reordered)) / SUM(q2_reordered) * 100, 2)
+            ELSE NULL
+        END AS total_percent_change
+    FROM(SELECT * FROM total_reorders_augmented WHERE aisle != 'missing' AND department = 'frozen')
+    GROUP BY aisle
+    ORDER BY total_difference)
+
+SELECT
+    aisle_total,
+    total_difference,
+    non_organic_difference,
+    organic_difference,
+    total_percent_change,
+    non_organic_percent_change,
+    organic_percent_change
+FROM total_aisle
+JOIN organic_aisle
+    ON aisle_total = aisle_organic
+JOIN non_organic_aisle
+    ON aisle_total = aisle_non_organic
+ORDER BY total_difference
---
+++ analysis_queries/product_counts.sql
@@ -0,0 +1,35 @@
+
+WITH organic_count AS
+    (SELECT 
+        department,
+        ROUND(COUNT(product_name), 2) AS organic_count
+    FROM (SELECT * FROM total_reorders_augmented WHERE product_name ILIKE '%organic%')
+    GROUP BY department),
+
+    non_organic_count AS
+    (SELECT 
+        department,
+        ROUND(COUNT(product_name), 2) AS non_organic_count
+    FROM (SELECT * FROM total_reorders_augmented WHERE product_name NOT ILIKE '%organic%')
+    GROUP BY department),
+
+    total_count AS
+    (SELECT 
+        department,
+        ROUND(COUNT(product_name), 2) AS total_count
+    FROM (SELECT * FROM total_reorders_augmented)
+    GROUP BY department)
+
+SELECT
+    tc.department,
+    total_count,
+    non_organic_count,
+    ROUND(non_organic_count/total_count * 100, 2) AS percent_products_non_organic,
+    organic_count,
+    ROUND(organic_count/total_count * 100, 2) AS percent_products_organic
+FROM total_count tc
+JOIN non_organic_count nc
+    ON tc.department = nc.department
+JOIN organic_count oc
+    ON tc.department = oc.department
+ORDER BY percent_products_organic DESC
